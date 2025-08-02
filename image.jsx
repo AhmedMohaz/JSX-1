50a94b6b26f2724802a06699fb76552be54ee49f
Ahmed Mohamed <itslokin5@gmail.com>
2025-08-02 11:37:41 +0300

    Create image.jsx

---
+++ image.jsx
@@ -0,0 +1,14 @@
+
+import React from 'react';
+import product from '../product';
+
+const Image = () => (
+  <img
+    src={product.image}
+    alt={product.name}
+    className="img-fluid rounded w-100"
+  />
+);
+
+export default Image;
+
